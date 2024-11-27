dc24b0739f6e950f170d5cbabe894cae911257da
Sriram <sriramsubramanian23@gmail.com>
2024-11-27 18:50:56 +0530

    Implemented structure for hero section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,24 @@
-const Home  = () => {
-    return (
-        <h1>Home</h1>
-    )
+import React from 'react';
+import { Link } from 'react-router-dom';
+import HERO_IMAGE from '../assets/hero.png'
+import "./Home.css"
+
+function Home() {
+  return (
+    <div className="hero-container">
+      <div className="hero-left">
+        <h1>Little Lemon</h1>
+        <h2>Bangalore</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+        <Link to="/reservation" className="reserve-button">
+          Reserve a Table
+        </Link>
+      </div>
+      <div className="hero-right">
+        <img src={HERO_IMAGE} alt="Hero Image" className="hero-image" />
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
